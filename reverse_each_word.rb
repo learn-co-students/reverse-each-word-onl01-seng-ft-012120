4880cf6dd049634f27e99883c2529d9b68fb31fe
Alehandro Persaud <andrew041994@gmail.com>
2020-01-23 16:44:18 +0000

    Done.

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,3 +1,6 @@
 def reverse_each_word(sentence1)
-  puts sentence1
+  array = sentence1.split
+  reversed_words = []
+  array.collect { |str| reversed_words << str.reverse }
+  reversed_words.join(" ")
 end
